208217b43576e5b670c73cc8e3b342013a21654a
jren44 <95770882+jren44@users.noreply.github.com>
2023-02-14 19:49:17 -0500

    Update clip_retrieval.py

--- clip_retrieval.py
+++ clip_retrieval.py
@@ -18,6 +18,7 @@ def compute_retrieval(model, images, refs, device, verbose=True):
             saved.add(image)
     images = unique_images
     refs = unique_refs
+    
     all_refs = []
     # img_labels is the corresponding image of the ref
     img_labels = []
@@ -33,68 +34,45 @@ def compute_retrieval(model, images, refs, device, verbose=True):
     text_features = extract_all_captions(
         all_refs, model.clip, device).to(device)
     if isinstance(model, clipscore.DNCLIPScore):
-        print("Using FirstClip")
         image_features = image_features - 0.25 * \
             torch.mean(image_features, dim=0)
         text_features = text_features - 0.25*torch.mean(text_features, dim=0)
 
     img_labels = torch.Tensor(img_labels).long()
+    
+    # Text->Images retrieval
     t2i_sim = (text_features @ image_features.T).cpu()
-    if "jlr" in str(Path.cwd()):
-        num_classes_img = t2i_sim.size(1)
-        i_top1 = Accuracy(top_k=1, task="multiclass",
-                          num_classes=num_classes_img)(t2i_sim, img_labels)
-        i_top5 = Accuracy(top_k=5, task="multiclass",
-                          num_classes=num_classes_img)(t2i_sim, img_labels)
-        i_top10 = Accuracy(top_k=10, task="multiclass",
-                           num_classes=num_classes_img)(t2i_sim, img_labels)
-    else:
-        i_top1 = Accuracy(top_k=1)(t2i_sim, img_labels)
-        i_top5 = Accuracy(top_k=5)(t2i_sim, img_labels)
-        i_top10 = Accuracy(top_k=10)(t2i_sim, img_labels)
-
-    # Images->Text
-    # sim = t2i_sim.T
-    # indexes = torch.argsort(sim, dim=1, descending=True)[:, :10]
-    # w, h = indexes.size()
-    # index_labels = torch.zeros(w, h).long()
-    # for i in range(w):
-    #     for j in range(h):
-    #         index_labels[i, j] = img_labels[indexes[i, j]]
-    # top1 = torch.mean(torch.where(
-    #     torch.sum(index_labels[:, :1] == torch.arange(w).reshape(-1, 1), dim=1) > 0, 1.0, 0.0))
-    # top5 = torch.mean(torch.where(
-    #     torch.sum(index_labels[:, :5] == torch.arange(w).reshape(-1, 1), dim=1) > 0, 1.0, 0.0))
-    # top10 = torch.mean(torch.where(
-    #     torch.sum(index_labels == torch.arange(w).reshape(-1, 1), dim=1) > 0, 1.0, 0.0))
-
-    # print("Yifei I2T")
-    # print(f"{top1=}, {top5=}, {top10=}")
-
-    i2t_sim = t2i_sim.T.cpu().numpy()
-    ranks = np.zeros(i2t_sim.shape[0])
-    for index, score in enumerate(i2t_sim):
-        inds = np.argsort(score)[::-1]
-        # Score
-        rank = 1e20
-        for i in txt_labels[index]:
-            tmp = np.where(inds == i)[0][0]
-            if tmp < rank:
-                rank = tmp
-        ranks[index] = rank
-
-    # Compute metrics
-    t_top1 = 100.0 * len(np.where(ranks < 1)[0]) / len(ranks)
-    t_top5 = 100.0 * len(np.where(ranks < 5)[0]) / len(ranks)
-    t_top10 = 100.0 * len(np.where(ranks < 10)[0]) / len(ranks)
-
+    num_classes_img = t2i_sim.size(1)
+    i_top1 = Accuracy(top_k=1, task="multiclass",
+                      num_classes=num_classes_img)(t2i_sim, img_labels)
+    i_top5 = Accuracy(top_k=5, task="multiclass",
+                      num_classes=num_classes_img)(t2i_sim, img_labels)
+    i_top10 = Accuracy(top_k=10, task="multiclass",
+                       num_classes=num_classes_img)(t2i_sim, img_labels)
+    t2i_results = [t_top1, t_top5, t_top10]
+    
+    # Images->Text retrieval
+    sim = t2i_sim.T
+    indexes = torch.argsort(sim, dim=1, descending=True)[:, :10]
+    w, h = indexes.size()
+    index_labels = torch.zeros(w, h).long()
+    for i in range(w):
+        for j in range(h):
+            index_labels[i, j] = img_labels[indexes[i, j]]
+    i_top1 = torch.mean(torch.where(
+        torch.sum(index_labels[:, :1] == torch.arange(w).reshape(-1, 1), dim=1) > 0, 1.0, 0.0))
+    i_top5 = torch.mean(torch.where(
+        torch.sum(index_labels[:, :5] == torch.arange(w).reshape(-1, 1), dim=1) > 0, 1.0, 0.0))
+    i_top10 = torch.mean(torch.where(
+        torch.sum(index_labels == torch.arange(w).reshape(-1, 1), dim=1) > 0, 1.0, 0.0))
+    i2t_results = [i_top1, i_top5, i_top10]
+    
     if verbose:
-        print(f'Top-1 Accuracy (Image): {i_top1}')
-        print(f'Top-5 Accuracy (Image): {i_top5}')
-        print(f'Top-10 Accuracy (Image): {i_top10}')
-        print(f'Top-1 Accuracy (Text): {t_top1}')
-        print(f'Top-5 Accuracy (Text): {t_top5}')
-        print(f'Top-10 Accuracy (Text): {t_top10}')
-    # preds = torch.argsort(sim, dim=1)
+        print(f'Top-1 Accuracy (Text->Image): {i_top1}')
+        print(f'Top-5 Accuracy (Text->Image): {i_top5}')
+        print(f'Top-10 Accuracy (Text->Image): {i_top10}')
+        print(f'Top-1 Accuracy (Image->Text): {t_top1}')
+        print(f'Top-5 Accuracy (Image->Text): {t_top5}')
+        print(f'Top-10 Accuracy (Image->Text): {t_top10}')
 
-    return [i_top1.item(), i_top5.item(), i_top10.item()], [t_top1, t_top5, t_top10]
+    return i2t_results, t2i_results
