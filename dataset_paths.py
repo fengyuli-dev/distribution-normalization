2ac80bfacd87d9140f6900e225825e19e4ba84f8
Yifei Zhou <1365684279@qq.com>
2023-02-14 23:06:05 -0500

    add dataset paths

---
+++ dataset_paths.py
@@ -0,0 +1,6 @@
+from pathlib import Path
+IMAGE_CAPTION_METRICS = Path('/share/cuvl/image_caption_metrics')
+CIFAR100_DIR = Path('/share/cuvl/image_caption_metrics/cifar-100-python')
+IMAGENET1K_DIR = Path('/share/cuvl/image_caption_metrics/imagenet-1k')
+SUN397_DIR = Path('/share/cuvl/image_caption_metrics/SUN397')
+STANFORDCARS_DIR = Path('/share/cuvl/image_caption_metrics/stanford_cars')
