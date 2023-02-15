1d4e656673790be4d55dce70998852cbe29665ec
Yifei Zhou <1365684279@qq.com>
2023-02-15 00:24:02 -0500

    finish_cleaningup_zeroshot

--- clip_zeroshot.py
+++ clip_zeroshot.py
@@ -29,24 +29,13 @@ import clipscore
 import json
 from torchmetrics import Accuracy
 import os
-from pathlib import Path
-from universal_mean import get_universal_embeddings
-
-IMAGE_CAPTION_METRICS = Path('/share/cuvl/image_caption_metrics')
-CIFAR100_DIR = '/share/cuvl/image_caption_metrics/cifar-100-python'
-IMAGENET1K_DIR = '/share/cuvl/image_caption_metrics/imagenet-1k'
-SUN397_DIR = '/share/cuvl/image_caption_metrics/SUN397'
-STANFORDCARS_DIR = '/share/cuvl/image_caption_metrics/stanford_cars'
 
 
-IMAGE_CAPTION_METRICS = Path('/share/cuvl/image_caption_metrics')
-CIFAR100_DIR = '/share/cuvl/image_caption_metrics/cifar-100-python'
-IMAGENET1K_DIR = '/share/cuvl/image_caption_metrics/imagenet-1k'
-SUN397_DIR = '/share/cuvl/image_caption_metrics/SUN397'
-STANFORDCARS_DIR = '/share/cuvl/image_caption_metrics/stanford_cars'
+# the coefficient for distribution normalization
+LAMBDA = 0.25
 
 
-def zeroshot_prediction(model, images, captions, labels, device, args):
+def zeroshot_prediction(model, images, val_images, captions, labels, device, args):
     model.eval().to(device)
     clip = model.clip
     clip.to(device)
@@ -54,89 +43,31 @@ def zeroshot_prediction(model, images, captions, labels, device, args):
         images, clip, device, normalize=False).cpu()
     caption_embeddings = clipscore.extract_all_captions(
         captions, clip, device, normalize=False).cpu()
-    if args.model == 'first':
-        # image_embeddings = image_embeddings - \
-        #     0.25*torch.mean(image_embeddings, dim=0)
-        # caption_embeddings = caption_embeddings - \
-        #     0.25*torch.mean(caption_embeddings, dim=0)
-        # image_mean, text_mean = get_mscoco_mean(model, device)
-        image_embeddings = image_embeddings - \
-            torch.mean(image_embeddings,  dim=0).cpu()*0.25
-        caption_embeddings = caption_embeddings - \
-            torch.mean(caption_embeddings, dim=0).cpu() * 0.25
-    image_embeddings = F.normalize(image_embeddings)
-    caption_embeddings = F.normalize(caption_embeddings)
-    num_classes = caption_embeddings.size(0)
-    if isinstance(model, clipscore.OriginalCLIPScore) or isinstance(model, clipscore.DNCLIPScore):
+    top1s = []
+    top5s = []
+    for _ in range(5):
+        _image_embeddings = image_embeddings
+        _caption_embeddings = caption_embeddings
+        if args.model == 'first':
+            val_embeddings = clipscore.extract_all_images(
+                random.sample(val_images, args.num_samples), clip, device, normalize=False).cpu()
+            _image_embeddings = image_embeddings - \
+                torch.mean(val_embeddings, dim=0).cpu()*LAMBDA
+            _caption_embeddings = caption_embeddings - \
+                torch.mean(caption_embeddings, dim=0).cpu() * LAMBDA
+        _image_embeddings = F.normalize(_image_embeddings)
+        _caption_embeddings = F.normalize(_caption_embeddings)
         sims = []
-        for i in range(0, image_embeddings.size(0), 32):
-            sims.append(image_embeddings[i:i+32] @ caption_embeddings.T)
+        for i in range(0, _image_embeddings.size(0), 32):
+            sims.append(_image_embeddings[i:i+32] @ _caption_embeddings.T)
 
-    if isinstance(model, clipscore.BatchCLIP):
-        sims = []
-        for i in range(image_embeddings.size(0)):
-            current_image = image_embeddings[i].broadcast_to(
-                caption_embeddings.size())
-            sims.append(model.train_forward(current_image,
-                        caption_embeddings).reshape(1, -1))
-    sims = torch.cat(sims, dim=0)
-    preds = torch.argmax(sims, dim=1)
-    # print(labels)
-    # print(preds)
-    acc = np.mean(preds.cpu().numpy() == np.array(labels))
-    print('Using OriginalCLIPSore')
-    print(f'Accuracy: {acc}')
-    return acc
-
-
-def zeroshot_train_fast(model, images, captions, labels, device,
-                        max_iter=20, learning_rate=1e-2):
-    """
-    enter the training loop for max_iter iterations
-    """
-    model.train().to(device)
-    clip = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
-    clip.to(device)
-    accuracy = Accuracy()
-    print("=====> Computing Image Embeddings")
-    image_features = clipscore.extract_all_images(images, clip, device).cpu()
-    text_features = clipscore.extract_all_captions(
-        captions, clip, device).cpu()
-    data_loader = torch.utils.data.DataLoader(
-        clipscore.CLIPJointDataset(image_features, labels),
-        batch_size=64, num_workers=1, shuffle=True)
-    criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam([p for p in model.embedding_fc.parameters()] +
-                                 [p for p in model.output_fc.parameters()] +
-                                 [p for p in model.text_embedding_fc.parameters()], lr=learning_rate)
-    embedding_size = image_features.size(1)
-    num_classes = text_features.size(0)
-    text = text_features.to(device).reshape(1, num_classes, -1)
-    print("=====> Training")
-    for i in tqdm.tqdm(range(max_iter)):
-        losses = []
-        accs = []
-        for d in data_loader:
-            images = d['image'].to(device)
-            batch_size = images.size(0)
-            images = images.reshape(batch_size, 1, -1)
-            # caption actually here contains labels
-            target = d['caption'].to(device).flatten()
-            images = images.broadcast_to(
-                (batch_size, num_classes, embedding_size))
-            images = images.reshape(-1, embedding_size).to(device)
-            batch_text = text[:].broadcast_to(
-                (batch_size, num_classes, embedding_size))
-            batch_text = batch_text.reshape(-1, embedding_size).to(device)
-            pred = model.train_forward(
-                images, batch_text).reshape(-1, num_classes)
-            loss = criterion(pred, target)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            acc = accuracy(torch.argmax(pred, dim=1).cpu(), target.cpu())
-            losses.append(loss.item())
-            accs.append(acc.item())
-        print(f"for iteration {i}: loss: {np.mean(losses)}")
-        print(f"for iteration {i}: Acc: {np.mean(accs)}")
-    return acc
+        sims = torch.cat(sims, dim=0)
+        top1 = Accuracy(top_k=1)(sims, torch.FloatTensor(labels).long())
+        top5 = Accuracy(top_k=5)(sims, torch.FloatTensor(labels).long())
+        top1s.append(top1*100)
+        top5s.append(top5*100)
+    print(f'Top-1 Accuracy: {np.mean(top1s)}')
+    print(f'Top-1 STD: {np.std(top1s)}')
+    print(f'Top-5 Accuracy: {np.mean(top5s)}')
+    print(f'Top-5 STD: {np.std(top5s)}')
+    return top1
--- compute_zeroshot.py
+++ compute_zeroshot.py
@@ -8,57 +8,116 @@ import warnings
 import torch
 import numpy as np
 import json
+import scipy
 import os
-import scipy.stats
 import clipscore
-import sys
-from clip_zeroshot import zeroshot_prediction
-from clip_zeroshot import IMAGENET1K_DIR, IMAGE_CAPTION_METRICS, CIFAR100_DIR, SUN397_DIR, STANFORDCARS_DIR
+import torch.nn.functional as F
+from dataset_paths import IMAGENET1K_DIR, IMAGE_CAPTION_METRICS, CIFAR100_DIR, SUN397_DIR, STANFORDCARS_DIR
+from torchmetrics import Accuracy
+import random
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 if device == 'cpu':
     warnings.warn('Running on CPU.')
+# the coefficient for distribution normalization
+LAMBDA = 0.25
+
+
+def zeroshot_prediction(model, images, val_images, captions, labels, device, args):
+    model.eval().to(device)
+    clip = model.clip
+    clip.to(device)
+    image_embeddings = clipscore.extract_all_images(
+        images, clip, device, normalize=False).cpu()
+    caption_embeddings = clipscore.extract_all_captions(
+        captions, clip, device, normalize=False).cpu()
+    top1s = []
+    top5s = []
+    # repeat experiments
+    for _ in range(args.num_experiments):
+        _image_embeddings = image_embeddings
+        _caption_embeddings = caption_embeddings
+        if args.dn:
+            # randomly sample args.num_samples images from the validation set
+            # to estimate the mean
+            val_embeddings = clipscore.extract_all_images(
+                random.sample(val_images, args.num_samples), clip, device, normalize=False).cpu()
+            # perforrm distribution normalization
+            _image_embeddings = image_embeddings - \
+                torch.mean(val_embeddings, dim=0).cpu() * LAMBDA
+            _caption_embeddings = caption_embeddings - \
+                torch.mean(caption_embeddings, dim=0).cpu() * LAMBDA
+        _image_embeddings = F.normalize(_image_embeddings)
+        _caption_embeddings = F.normalize(_caption_embeddings)
+        sims = []
+        for i in range(0, _image_embeddings.size(0), 32):
+            sims.append(_image_embeddings[i:i+32] @ _caption_embeddings.T)
+
+        sims = torch.cat(sims, dim=0)
+        top1 = Accuracy(top_k=1)(sims, torch.FloatTensor(labels).long())
+        top5 = Accuracy(top_k=5)(sims, torch.FloatTensor(labels).long())
+        top1s.append(top1*100)
+        top5s.append(top5*100)
+    print(f'Top-1 Accuracy: {np.mean(top1s)}')
+    print(f'Top-1 STD: {np.std(top1s)}')
+    print(f'Top-5 Accuracy: {np.mean(top5s)}')
+    print(f'Top-5 STD: {np.std(top5s)}')
+    return top1
 
 
 def main(args):
     print(f'{args.dataset} (Zero-shot Accuracy)')
-    model_name = args.model
-    if model_name == 'regular' or model_name == 'first':
-        model = clipscore.OriginalCLIPScore()
-    elif model_name == 'batch':
-        model = clipscore.BatchCLIP()
+    model = clipscore.OriginalCLIPScore()
     model.to(device)
 
     images = []
     labels = []
     captions = []
+    val_images = []
+
+    # load cifar100 dataset
     if args.dataset == 'cifar100':
-        with open(CIFAR100_DIR + '/str_labels.json', 'r') as f:
+        with open(Path(CIFAR100_DIR, 'str_labels.json'), 'r') as f:
             str_labels = json.load(f)
         captions = [f'a photo of a {s}.' for s in str_labels]
-        validation_dir = os.path.join(CIFAR100_DIR, 'validation')
+        test_dir = Path(CIFAR100_DIR, 'validation')
         for label in range(100):
-            image_folder = validation_dir + '/' + str(label)
+            image_folder = Path(test_dir, str(label))
             current_images = os.listdir(image_folder)
             for image in current_images:
-                images.append(image_folder + '/' + image)
+                images.append(Path(image_folder, image).as_posix())
                 labels.append(label)
+        validation_dir = Path(CIFAR100_DIR, 'training')
+        for label in range(100):
+            image_folder = Path(validation_dir, str(label))
+            current_images = os.listdir(image_folder)
+            for image in current_images:
+                val_images.append(Path(image_folder, image).as_posix())
 
+    # load imagenet1k dataset
     if args.dataset == 'imagenet1k':
-        with open(IMAGENET1K_DIR + '/str_labels.json', 'r') as f:
+        with open(Path(IMAGENET1K_DIR, 'str_labels.json'), 'r') as f:
             str_labels = json.load(f)
         captions = [f'a photo of a {s}.' for s in str_labels]
-        training_dir = os.path.join(IMAGENET1K_DIR, 'validation')
+        test_dir = Path(IMAGENET1K_DIR, 'validation')
         for label in range(1000):
-            image_folder = training_dir + '/' + str(label)
+            image_folder = Path(test_dir, str(label))
             current_images = os.listdir(image_folder)
             for image in current_images:
-                images.append(image_folder + '/' + image)
+                images.append(Path(image_folder, image).as_posix())
                 labels.append(label)
 
+        validation_dir = Path(IMAGENET1K_DIR, 'training')
+        for label in range(1000):
+            image_folder = Path(validation_dir, str(label))
+            current_images = os.listdir(image_folder)
+            for image in current_images:
+                val_images.append(Path(image_folder, image).as_posix())
+
+    # load sun397 dataset
     if args.dataset == 'sun397':
         class_names = []
-        with open(SUN397_DIR + '/ClassName.txt', 'r') as fb:
+        with open(Path(SUN397_DIR, 'ClassName.txt'), 'r') as fb:
             for line in fb:
                 class_names.append(line.replace(
                     '\n', '').strip())
@@ -66,39 +125,42 @@ def main(args):
             f'a photo of a {s.replace("_", " ").split("/")[2]}.' for s in class_names]
         # print(captions)
         for i, class_name in enumerate(class_names):
-            image_folder = SUN397_DIR + class_name
-            current_images = os.listdir(image_folder)[-50:]
-            for image in current_images:
-                images.append(image_folder + '/' + image)
+            image_folder = Path(SUN397_DIR, class_name[1:])
+            test_images = os.listdir(image_folder)[-50:]
+            validation_images = os.listdir(image_folder)[:50]
+            for image in test_images:
+                images.append(Path(image_folder, image).as_posix())
                 labels.append(i)
+            for image in validation_images:
+                val_images.append(Path(image_folder, image).as_posix())
 
+    # load stanford_cars dataset
     if args.dataset == 'stanford_cars':
-        with open(STANFORDCARS_DIR + '/str_labels.json', 'r') as f:
+        with open(Path(STANFORDCARS_DIR, 'str_labels.json'), 'r') as f:
             classes = json.load(f)
         captions = [f'a photo of a {s}' for s in classes]
-        with open(STANFORDCARS_DIR + '/devkit/train_perfect_preds.txt', 'r') as f:
-            for line in f:
-                labels.append(int(line.replace('\n', '').strip())-1)
-        image_folder = STANFORDCARS_DIR + '/cars_train'
-        current_images = sorted(os.listdir(image_folder))
-        for image in current_images:
-            images.append(image_folder + '/' + image)
+        image_folder = Path(STANFORDCARS_DIR, 'cars_test')
+        test_data = scipy.io.loadmat(
+            Path(STANFORDCARS_DIR, 'cars_test_annos_withlabels.mat'))['annotations'][0]
+        for d in test_data:
+            images.append(Path(image_folder, d[5][0]).as_posix())
+            labels.append(d[4][0][0] - 1)
+        val_data = scipy.io.loadmat(
+            Path(STANFORDCARS_DIR, 'devkit/cars_train_annos.mat'))['annotations'][0]
+        image_folder = Path(STANFORDCARS_DIR, 'cars_train')
+        for d in val_data:
+            val_images.append(Path(image_folder, d[5][0]).as_posix())
 
-        # load the model
-    if model_name == 'batch':
-        model.load_state_dict(torch.load('BatchCLIP.pt')['model_state_dict'])
-
-    acc = zeroshot_prediction(model, images, captions, labels, device, args)
+    zeroshot_prediction(
+        model, images, val_images, captions, labels, device, args)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataset', default="cifar100",
                         choices=["cifar100", "imagenet1k", "sun397", "stanford_cars"], type=str)
-    parser.add_argument('--model', default='first',
-                        choices=['batch', 'regular', 'first'], type=str)
-    parser.add_argument('--universal_mean', action="store_true", default=False)
-    parser.add_argument("--normalize", action="store_true", default=False, help="normalize then take mean")
-    parser.add_argument('-s', '--scalar', type=float, default=0.25)
+    parser.add_argument('--dn', action="store_true", default=False)
+    parser.add_argument('--num_samples', default=100, type=int)
+    parser.add_argument('--num_experiments', default=5, type=int)
     args = parser.parse_args()
     main(args)
